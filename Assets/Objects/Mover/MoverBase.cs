393fa35aba64b400dd74554cb114f246604f4e3a
HuvaaKoodia <hyvaakoodia@gmail.com>
2013-05-17 18:46:05 +0300

    Random content commit

---
+++ Assets/Objects/Mover/MoverBase.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+
+public class MoverBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+	
+		var dir=Vector3.zero;
+		
+		if (Input.GetKey(KeyCode.W)){
+			dir=Vector3.forward;
+		}
+		
+		if (Input.GetKey(KeyCode.S)){
+			dir=Vector3.back;
+		}
+		
+		if (Input.GetKey(KeyCode.A)){
+			dir=Vector3.left;
+		}
+		
+		if (Input.GetKey(KeyCode.D)){
+			dir=Vector3.right;
+		}
+		
+		if (Input.GetKey(KeyCode.Space)){
+			dir=Vector3.up;
+		}
+		
+		if (Input.GetKey(KeyCode.LeftControl)){
+			dir=Vector3.down;
+		}
+		
+		Move(dir,0.1f);
+	}
+	
+	void Move(Vector3 dir,float speed){
+		transform.position+=dir*speed;	
+	}
+}
